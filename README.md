526a6234690f060577320ac8eeef51f11e7f9722
Josh2217 <JoshdJohnson2217@gmail.com>
2022-04-18 01:14:28 -0400

    Playing around with JavaFX

---
+++ README.md
@@ -0,0 +1,21 @@
+# Generative Art
+
+This entire repo is solely for Assignment 9 of CSC 201; however, I will probably end up continuing to work on it because I've come to love the look of generative art.
+
+--Josh
+
+# Assignment 9
+
+Set the Stage Title to be your first and last name.
+
+Create some simple generative art
+
+* Your program must use one or more of the shape classes
+
+* Your program must use one or more colors
+
+* Your program must use java.util.Random to make the output change every time your program is run
+
+__**Don't Plagiarize**__
+
+In your submission email, describe your program and it's behavior. 
--- src/main/java/josh/Assignment9.java
+++ src/main/java/josh/Assignment9.java
@@ -5,7 +5,10 @@ package josh;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.*;
+import javafx.scene.text.*;
 import javafx.stage.Stage;
 
 public class Assignment9 extends Application{
@@ -21,12 +24,20 @@ public class Assignment9 extends Application{
         */
 
         Group root = new Group();
-        Scene scene = new Scene(root, new Color(0.8, 0.8, 0.8, 0.8));
+        Scene scene = new Scene(root, 1280, 720, new Color(0.1, 0.1, 0.1, 1));
+        
+        Image icon = new Image(Assignment9.class.getResourceAsStream("duckhaert.png"));
+        Text text = new Text(100., 100., "Josh Johnson");
+        text.setFont(new Font("Consolas", 48));
+        text.setFill(Color.WHITE);
+        root.getChildren().add(text);
+
+        stage.getIcons().add(icon);
         stage.setScene(scene);
-
-
-        stage.setTitle("Assignment 9");
+        stage.setTitle("Josh Johnson: Generative Art");
+        stage.setAlwaysOnTop(true);
         stage.show();
+        stage.setAlwaysOnTop(false);
     }
 
     public static void main(String[] args) {
